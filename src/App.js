2888188553aa6d56eb71575ae333552e57f13348
sameera <abeysekaramadushanka14@gmail.com>
2021-08-27 02:33:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState(null);
+  const [searchInfo, setSearchInfo] = useState("");
+
+  const handleSearch =  (value) => {
+    setSearch(value);
+    if(value){
+      setSearchInfo(value.includes('v=')?value.split("v=")[1].split("&")[0]:"");
+    }
+  }
+
+  const loadLinks = async (value) =>{
+    console.log(value)
+    let endpoint="";
+    if(value =="Audio"){
+      endpoint = `https://api.vevioz.com/@api/button/mp3/${searchInfo}`;
+    }else if(value == "Video"){
+      endpoint = `https://api.vevioz.com/@api/button/videos/all/${searchInfo}`;
+    }
+    let finalUrl = `<iframe src="${endpoint}" scrolling="no" class="downloadFrame"></iframe>`;
+    setResults(finalUrl);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div >
+
+      <div className="card-container">
+        {/* <span class="pro">PRO</span> */}
+        <h6>Sam's YTube Downloader</h6>
+
+        {(searchInfo) ? 
+        <iframe className='video'
+        sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
+        src={`https://www.youtube.com/embed/${searchInfo}`}>
+        </iframe>
+        : ''}
+
+        <p className="textInput">
+          <input
+            type="search"
+            placeholder="Paste your youTube link here"
+            value={search}
+            onChange={e => handleSearch(e.target.value)} />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+        <div className="buttons">
+          <button className="primary" onClick={() => loadLinks('Audio')}>
+            Audio
+          </button>
+          <button className="primary" onClick={() => loadLinks('Video')}>
+            Video
+          </button>
+        </div>
+
+       
+
+        <div className="skills">
+          {(results)?<h3>Available Types</h3>:""}
+          <div dangerouslySetInnerHTML= {{ __html:results }}/></div>
+      </div>
+
     </div>
   );
 }
